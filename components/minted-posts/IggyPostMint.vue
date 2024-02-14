eb3ee8d21f778778a17dd966568d75c6fc4a7712
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-02-14 19:32:12 +0100

    watch chain id

--- components/minted-posts/IggyPostMint.vue
+++ components/minted-posts/IggyPostMint.vue
@@ -275,6 +275,15 @@ export default {
     return {
       address, chainId, isActivated, signer, toast, userStore
     }
+  },
+
+  watch: {
+    chainId() {
+      if (this.isSupportedChain) {
+        this.fetchMintData();
+      }
+    }
   }
+
 }
 </script>
