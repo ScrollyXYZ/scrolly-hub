a0ee917a9950f14b4f30506cf0f01c81ac08ac87
ScrollyXYZ <scrollythemap@gmail.com>
2024-04-20 22:40:44 +0200

    Souncloud Add

--- components/sidebars/SidebarRight.vue
+++ components/sidebars/SidebarRight.vue
@@ -87,7 +87,8 @@
             {{ $config.projectName }} Playlist
           </div>
           <div class="card-body sidebar-card-body">
-            <iframe
+          <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1812713007&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/scrolly-themap" title="Scrolly TheMap" target="_blank" style="color: #cccccc; text-decoration: none;">Scrolly TheMap</a> · <a href="https://soundcloud.com/scrolly-themap/sets/scrolliverse-song" title="Scrolliverse Song" target="_blank" style="color: #cccccc; text-decoration: none;">Scrolliverse Song</a></div>
+            <!-- <iframe
               style="border-radius: 12px"
               :src="
                 'https://open.spotify.com/embed/playlist/' +
@@ -100,7 +101,7 @@
               allowfullscreen=""
               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
               loading="lazy"
-            ></iframe>
+            ></iframe> -->
           </div>
         </div>
 
